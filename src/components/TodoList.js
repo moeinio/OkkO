7e9af1e30e2decb97fcc00cbb1ad8ec0cb21ddfd
Saeid Rezaei <torghoundy@gmail.com>
2021-08-03 12:03:29 +0200

    Update TodoList.js
    migrated to function component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 import TodoElement from "./TodoElement";
 
-export default class TodoList extends React.Component {
-    render() {
-        return (
-            <TodoContainer>
-                {this.props.todos.length > 0 &&
-                    <>
-                    {this.props.todos.map(todo => <TodoElement onRemove={this.props.removeTodo} updateTodo={this.props.updateTodo} key={todo.id} todo={todo} />)}
-                    </>
-                }
-            </TodoContainer>
-        )
-    };
-}
+const TodoList = (props) => {
+  return (
+    <TodoContainer>
+      {props.todos.length > 0 && (
+        <>
+          {props.todos.map((todo) => (
+            <TodoElement
+              onRemove={props.removeTodo}
+              updateTodo={props.updateTodo}
+              key={todo.id}
+              todo={todo}
+            />
+          ))}
+        </>
+      )}
+    </TodoContainer>
+  );
+};
+
+export default TodoList;
 const TodoContainer = styled.div`
   font-size: 1em;
   display: flex;
