351db88f2b67ba860cfd854f012f68a8b2d7cfd5
mOEiNiO <torghoundy@gmail.com>
2021-08-12 02:01:26 +0200

    modified syntax

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,22 +1,18 @@
-import React, {useState} from 'react';
-
+import React, { useState } from "react";
 
 const TextInput = () => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    textChange = (e) => {
-        setText(e.target.value )
-    };
+  const textChange = (e) => {
+    setText(e.target.value);
+  };
 
-    render() {
-        return (
-            <div>
-                <input onChange={(e) => this.textChange(e)} />
-                {text}
-            </div>
-        )
-    };
-}
-
+  return (
+    <div>
+      <input onChange={(e) => textChange(e)} />
+      {text}
+    </div>
+  );
+};
 
 export default TextInput;
